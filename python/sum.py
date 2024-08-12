c18b8f835d5d649a2d5237e306ba100e23ecc394
SK Sudipt <88238223+Sudipt111@users.noreply.github.com>
2024-08-12 20:13:27 +0530

    Delete python directory

--- python/sum.py
+++
@@ -1,4 +0,0 @@
-a=int(input("no1: "))
-b=int(input("no2: "))
-sum=a+b
-print("sum= ",sum)
--- python/vol_sphere.py
+++
@@ -1,3 +0,0 @@
-radius=float(input("enter the radius:" ))
-volume=(4/3*3.141)*radius**3
-print("volume is ",volume)
