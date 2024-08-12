b50516331c5e0218b6cc68f88c678cbe1682a1de
SK Sudipt <88238223+Sudipt111@users.noreply.github.com>
2024-08-12 20:12:30 +0530

    Delete HTML+CSS+JS directory

--- HTML+CSS+JS/P-4/script.js
+++
@@ -1,7 +0,0 @@
-// script.js
-document.addEventListener('DOMContentLoaded', (event) => {
-    const link = document.getElementById('link');
-    link.addEventListener('click', () => {
-        console.log('Link clicked!');
-    });
-});
